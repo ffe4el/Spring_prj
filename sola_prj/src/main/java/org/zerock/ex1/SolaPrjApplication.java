2ac7bb850ee72123f02031860377c52fa06fd3a8
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-04-05 16:02:32 +0900

    27p

---
+++ sola_prj/src/main/java/org/zerock/ex1/SolaPrjApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolaPrjApplication.class, args);
+    }
+
+}
