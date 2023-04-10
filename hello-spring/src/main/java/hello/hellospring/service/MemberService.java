8fb09f1b9a70fd114c71af6b74640431b580d5eb
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-04-10 23:12:15 +0900

    TDD 정신나갈것 같아...

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,10 +9,16 @@ import java.util.Optional;
 public class MemberService {
     //private final을 선언한 변수를 사용하면 재할당하지 못하며, 해당 필드,
     //메서드 별로 호출할 때마다 새로이 값이 할당(인스턴스화)한다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //위에 memberRepository에서 커맨드+n 눌러서 constructor 만들기
+    //외부에서 new memberRepository를 생성하게끔 하기
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원가입
-    public Long join(Member member){
+    public Long join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
@@ -20,17 +26,17 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
-                .ifPresent(m->{
+                .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
-//    전체 회원 조회
-    public List<Member> findMembers(){
+    //    전체 회원 조회
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
