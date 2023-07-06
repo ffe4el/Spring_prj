64205e46c7888cf18fc38c1645fef147e541d709
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-07-06 19:53:40 +0900

    구현체가 DIP를 위반

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        //1L은 long타입이어서 붙여준것이기 때문에 안붙이고 그냥 1이라고 치면 오류가남
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+
+}
--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,15 @@
 package hello.core.member;
 
+
+//MemberServiceImpl은 MemberRepository에도 의존하고 MemoryMemberRepository에도 의존함
 public class MemberServiceImpl implements MemberService{
 
+    //이부분에서 DIP 위배, 실제 할당하는 부분이 구현체를 의존함
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
     public void join(Member member) {
-
+        memberRepository.save(member);
     }
 
     @Override
