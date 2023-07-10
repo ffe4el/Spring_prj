88f8bd50eb7ec90f3ce2b609228220176ae8ccf9
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-07-10 18:34:34 +0900

    컴포넌트 스캔

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,13 +1,16 @@
 package hello.core.member;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 //MemberServiceImpl은 MemberRepository에도 의존하고 MemoryMemberRepository에도 의존함
 public class MemberServiceImpl implements MemberService{
 
-    //이부분에서 DIP 위배, 실제 할당하는 부분이 구현체를 의존함
     private final MemberRepository memberRepository;
 
-
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {//생성자를 통해서..
         this.memberRepository = memberRepository;
     }
