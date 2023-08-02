1e4e89f7c583d1f29575e3f815bdc8f2cb0aba33
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-08-02 15:25:09 +0900

    응답 메세지

--- servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -13,6 +13,15 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+
+/**
+ * http://localhost:8080/request-body-json
+ *
+ * JSON 형식 전송
+ * content-type: application/json
+ * message body: {"username": "hello", "age": 20}
+ *
+ */
 @WebServlet(name="requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
     private ObjectMapper objectMapper = new ObjectMapper();
