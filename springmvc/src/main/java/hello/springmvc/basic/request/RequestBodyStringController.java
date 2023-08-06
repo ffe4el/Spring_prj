326a629235d4baa2fe2c8e429959aa550612017b
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-08-06 18:10:32 +0900

    .

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -4,15 +4,19 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 /**
  * 요청 파라미터와 다르게, HTTP 메시지 바디를 통해 데이터가 직접 넘어오는 경우는
+ *
  * @RequestParam ,@ModelAttribute 를 사용할 수 없다.
  */
 @Slf4j
@@ -29,5 +33,51 @@ public class RequestBodyStringController {
         response.getWriter().write("ok");
 
     }
+
+    /**
+     * InputStream(Reader) : Http 요청 메시지 바디의 내용을 직접 조회
+     * OutputStream(Writer) : Http 응답 메시지의 바디에 직접 결과 출력
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info("messageBody={}", messageBody);
+        responseWriter.write("ok");
+    }
+
+
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * -메시지 바디 정보를 직접 조회 (@RequestParam X , @ModelAttribute X)
+     * -httpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     * <p>
+     * 응답에서도 HttpEntity 사용 가능
+     * - 메시지 바디 정보 직접 반환 (view 조회 X)
+     * - HttpMessageConverter 사용 -> StringHttpConverter 적용
+     */
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+        return new HttpEntity<>("ok");
+    }
+
+
+    /**
+     * @RequestBody 를 사용하면 HTTP 메시지 바디 정보를 편리하게 조회할 수 있다.
+     * @RequestHeader 를 사용하면 HTTP 메시지 헤더 정보를 편리하게 조회할 수 있다.
+     */
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
+        log.info("messageBody={}", messageBody);
+        return "ok";
+    }
+
+//    요청 파라미터 vs HTTP 메시지 바디
+//    요청 파라미터를 조회하는 기능: @RequestParam , @ModelAttribute
+//    HTTP 메시지 바디를 직접 조회하는 기능: @RequestBody
+
+
 
 }
