b22c4509d0cdff33f8649cc835526ddd87fc8b11
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-08-08 22:50:12 +0900

    상품 서비스 개발 완룡...!

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -86,9 +87,28 @@ public class BasicItemController {
     /**
      * PRG - post/redirect/get
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
     }
+
+
+    /**
+     * RedirectAttributes
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+        //http://localhost:8080/basic/items/3?status=true
+
+        //redirect:/basic/items/{itemId}
+        //pathVariable 바인딩: {itemId}
+        //나머지는 쿼리 파라미터로 처리: ?status=true
+    }
+
+
 }
